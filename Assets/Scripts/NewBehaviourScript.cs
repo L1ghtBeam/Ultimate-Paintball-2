d3989ee3cf5d76ded8c24b9a7853f830f004e260
Travis <travissilva44@gmail.com>
2020-01-28 09:37:54 -0800

    Multiplayer Team System
    script that might work to set players on to teams

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿var playerTeam1 : Transform;
+var playerTeam2 : Transform;
+var spawnpoint1 : GameObject;
+var spawnpoint2 : GameObject;
+
+ 
+ function OnGUI () {
+ if (GUI.Button (new Rect(10, 10, 100, 30), "Team Name1")) {
+    Network.Instantiate(playerTeam1, spawnpoint1.transform, spawnpoint1.rotation);
+ }
+ if (GUI.Button (new Rect(10, 50, 100, 30), "Team Name2")) {
+    Network.Instantiate(playerTeam2, spawnpoint2.transform, spawnpoint2.rotation);
+ }
+}
